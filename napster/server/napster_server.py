9332534b09f3edbe553253bff1563a15fa5cf666
Luca Ferrari <risinglf@gmail.com>
2012-03-07 11:26:15 +0100

    -Server napster ora sembra andare con thread, da testare

--- napster/server/napster_server.py
+++ napster/server/napster_server.py
@@ -1,15 +1,19 @@
 from managers.filesmanager import FilesManager
 from managers.usersmanager import UserManager
 import socket, os
+from threading import Thread
 
-class ServiceThread:
-    def __init__(self,socket):
+
+class ServiceThread(Thread):
+    def __init__(self, socket):
         self._socket = socket
+        super(ServiceThread, self).__init__()
 
     def login_user(self, ipp2p, pp2p):
-        UserManager.create_user("123123", "ssss")
+        #UserManager.create_user("123123", "ssss")
         print "tdb"
-        #return session_id
+        session_id = "1234567812345678"
+        return session_id
 
     def logout_user(self, socket):
         print "tdb"
@@ -32,35 +36,36 @@ class ServiceThread:
         #return download_number
 
     def run(self):
+        print "trhead started"
         while 1:
             try:
-                command = str(self._socket.read(4))
+                command = str(self._socket.recv(4))
 
                 if command == "LOGI":
                     peer_ip = str(self._socket.recv(15))
                     peer_port = str(self._socket.recv(5))
                     session_id = str(self.login_user(peer_ip, peer_port))
-                    socket.send("ALGI"+session_id)
+                    self._socket.send("ALOG"+session_id)
 
                 elif command == "ADDF":
                     peer_session_id = str(self._socket.recv(16))
                     file_md5 = str(self._socket.recv(16))
                     file_name = str(self._socket.recv(100))
                     copy_num = self.add_file(peer_session_id, file_md5, file_name)
-                    socket.send("AADD"+copy_num)
+                    self._socket.send("AADD"+copy_num)
 
                 elif command == "DELF":
                     peer_session_id = str(self._socket.recv(16))
                     file_md5 = str(self._socket.recv(16))
                     copy_num = self.remove_file(peer_session_id, file_md5)
-                    socket.send("ADEL"+copy_num)
+                    self._socket.send("ADEL"+copy_num)
 
                 elif command == "FIND":
                     peer_session_id = str(self._socket.recv(16))
                     query_string = str(self._socket.recv(20))
                     result = self.find_file(peer_session_id, query_string)
                     print "tbd"
-                    #socket.send("AFIN"+)
+                    #self._socket.send("AFIN"+)
 
                 elif command == "RREG":
                     peer_session_id = str(self._socket.recv(16))
@@ -68,25 +73,24 @@ class ServiceThread:
                     peer_ip = str(self._socket.recv(15))
                     peer_port = str(self._socket.recv(5))
                     download_num = self.register_download(peer_session_id, file_md5, peer_ip, peer_port)
-                    socket.send("ARRE"+download_num)
+                    self._socket.send("ARRE"+download_num)
 
                 elif command == "LOGO":
                     peer_session_id = str(self._socket.recv(16))
                     delete_num = self.find_file(peer_session_id, query_string)
-                    socket.send("ALGO"+delete_num)
-
+                    self._socket.send("ALGO"+delete_num)
 
             except Exception, e:
                 print e
                 break
 
 
-class NapsterServer:
+class NapsterServer(object):
 
     def __init__(self):
         self.server_socket = None
         print "Init Napster server"
-        server_socket = socket.socket(
+        self.server_socket = socket.socket(
             socket.AF_INET,
             socket.SOCK_STREAM
         )
@@ -101,12 +105,8 @@ class NapsterServer:
         print "Started, waiting for connection"
         while 1:
             (socket_client, address) = self.server_socket.accept()
-
-            socket_client.recv
-
-
-
-
+            s = ServiceThread(socket_client)
+            s.start()
 
 
 if __name__ == "__main__":

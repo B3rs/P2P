6860d64a78ffb1aeb0dfa6042ea01170099ba6ca
Francesca Guiducci <guiducci.francesca@gmail.com>
2012-03-29 10:56:39 +0200

    teamFGM: prima bozza del gnutella peer

--- gnutella/teamFGM/gnutella.py
+++ gnutella/teamFGM/gnutella.py
@@ -2,12 +2,12 @@ __author__ = 'ingiulio'
 
 import socket
 import hashlib #per calcolare l'md5 dei file
-import time # per le funzioni di wait -> uso le sleep, che mi freezano il processo
 import sys # mi consente di usare il metodo sys.stdout.write per scrivere sulla stessa riga
 import copy
 import string
 import random
 import time
+import gnutella_thread
 
 class GnutellaPeer(object):
 
@@ -33,7 +33,7 @@ class GnutellaPeer(object):
         self.stop = False #non voglio uscire subito dal programma
 
         # CREO LA SOCKET PER GLI ALTRI PEERS
-        #self.myserver = gnutella_peer_threads.ListenToPeers(self.my_IP, self.my_port)
+        #self.myserver = gnutella_threads.ListenToPeers(self.my_IP, self.my_port)
         #self.myserver.start()
 
         #vicini onnipresenti
@@ -106,27 +106,29 @@ class GnutellaPeer(object):
 
 
     def addNeighbour(self, IP, port):
-        if not sizeof(self.neighTable): #TODO: controllare che si possa fare
+        i=0
+        if len(self.neighTable) < self.dim_neighTable: #se tabella non ancora tutta piena
             newline = []
             newline.append(IP)
             newline.append(port)
             newline.append(time.time()) #numero di secondi dall'epoca (?)
             self.neighTable.append(newline) #inserisco la nuova riga nella tabella dei vicini
-            print self.neighTable #TODO debug
-        elif sizeof(self.neighTable) == self.dim_neighTable :
+        elif len(self.neighTable) == self.dim_neighTable : #tabella piena
             # devo eliminare qualche vicino memorizzato
-            self.new_neighbour = []
             to_replace = 0
-            oldest=self.neighTable[to_replace[2]] #inizializzo la variabile "piu' vecchio alla prima entry della tabella
-            for i in self.neighTable: # i e' un elemento di neighTable
-                tmp=self.neighTable[i[2]]
-                if tmp > oldest :
+            oldest=self.neighTable[to_replace][2] #inizializzo la variabile "piu' vecchio alla prima entry della tabella
+            for i in range(0,len(self.neighTable)): # i e' un elemento di neighTable
+                tmp=self.neighTable[i][2] #tmp diventa l'orario
+                if tmp < oldest :
                     oldest = tmp
                     to_replace = i
             # ho individuato l'elemento che non uso da piu' tempo e lo rimpiazzo con un neighbour nuovo
-            self.neighTable[to_replace[0]]= IP
-            self.neighTable[to_replace[1]]= port
-            self.neighTable[to_replace[2]]= time.time()
+            self.neighTable[to_replace][0]= IP
+            self.neighTable[to_replace][1]= port
+            self.neighTable[to_replace][2]= time.time()
+        else:
+            print "Errore nella gestione di neighTable"
+        print self.neighTable #TODO debug
     #end of method addNeighbour
 
 
@@ -162,6 +164,7 @@ class GnutellaPeer(object):
     def findFile(self):
 
         print "Find files...\n"
+
         query_TTL = "0" #inizializzazione fittizia per il while di controllo stdin
         search = raw_input("Insert a search string: ")
         search_form = '%(#)020s' % {"#" : search} #formatto la stringa di ricerca
@@ -169,13 +172,13 @@ class GnutellaPeer(object):
             query_TTL = raw_input("Insert TTL for query request (min=1, typ=4 or 5): ")
         query_TTL_form = '%(#)02d' % {"#" : int(query_TTL)}
         pktID = self.generate_pktID()
-        print "pktID generated for query flooding: " + pktID
+        print "pktID generated for query flooding files: " + pktID
         # invio la richiesta di query flooding a tutti i miei vicini
-        for n in self.neighTable: #n e' un elemento di neighTable
-            neigh_sock = self.openConn(self.neighTable[n[0]], self.neighTable[n[1]]) #passo ip e porta
-            neigh_sock.send("QUER" + pktID + self.my_IP_form + self.my_port_form + neigh_TTL_form + search_form)
-
-        # inserire sezione di attesa e controllo risposte
+        for n in range(0,len(self.neighTable)):
+            #neigh_sock = self.openConn(self.neighTable[n][0], self.neighTable[n][1]) #passo ip e porta
+            #neigh_sock.sendall("QUER" + pktID + self.my_IP_form + self.my_port_form + query_TTL_form + search_form)
+            print "QUER" + str(pktID) + self.my_IP_form + str(self.my_port_form) + query_TTL_form + search_form
+            #self.closeConn(neigh_sock)
 
     # end of findFile method
 
@@ -183,37 +186,21 @@ class GnutellaPeer(object):
     def findNeigh(self):
 
         print "Find neighbours..."
+
         neigh_TTL = "0" #inizializzazione fittizia per il while di conttollo stdin
         while int(neigh_TTL) < 1: #verifico che non venga inserito un valore non possibile
             neigh_TTL = raw_input("Insert neighbours TTL (min=1, typ=2): ")
         neigh_TTL_form = '%(#)02d' % {"#" : int(neigh_TTL)}
         pktID =  self.generate_pktID()
-        print "pktID generated for query flooding: " + pktID
+        print "pktID generated for query flooding neighbours: " + pktID
         # invio la richiesta di query flooding a tutti i miei vicini
-        for n in self.neighTable: #n e' un elemento di neighTable
-            neigh_sock = self.openConn(self.neighTable[n[0]], self.neighTable[n[1]]) #passo ip e porta
-            neigh_sock.send("NEAR" + pktID + self.my_IP_form + self.my_port_form + neigh_TTL_form)
-            #TODO: verificare che vada bene così, perchè posso ricevere anche piu' di una risposta
-            ack=self.sockRead(neigh_sock,20)
-            if ack[:16] == "ANEA":
-                print "OK, ack received" #TODO: debug
-                if ack[16:20] == pktID: #controllo che il pktID sia lo stesso di quello che ho generato io per la richiesta
-                    data = self.sockRead(neigh_sock,20)
-                    neigh_IP = data[:15]
-                    neigh_port = data[15:20]
-                    self.addNeighbour(neigh_IP,neigh_port)
-
-                    #TODO implementare funzione per eventuale aggiunta neighbours alla neighTable
-            else:
-                print "Expecting 'ANEA': ack parsing failed"
-                break
-            self.closeConn(neigh_sock)
+        for n in range(0,len(self.neighTable)):
+            #neigh_sock = self.openConn(self.neighTable[n][0], self.neighTable[n][1]) #passo ip e porta
+            #neigh_sock.sendall("NEAR" + pktID + self.my_IP_form + self.my_port_form + neigh_TTL_form)
+            print "NEAR" + str(pktID) + self.my_IP_form + str(self.my_port_form) + neigh_TTL_form
+            #self.closeConn(neigh_sock)
 
     # end of findNeigh method
-
-
-
-
 
 
     def goOut(self):
@@ -260,7 +247,6 @@ class GnutellaPeer(object):
 if __name__ == "__main__":
 
     gp = GnutellaPeer() #inizializzazione
-
 
     while not gp.stop:
 
---
+++ gnutella/teamFGM/gnutella_thread.py
@@ -0,0 +1,5 @@
+__author__ = 'ingiulio'
+
+import gnutella_service
+
+

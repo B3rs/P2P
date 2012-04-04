b34ac94f296f023b1ca2ef04d1870b97c9edb8f7
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2012-04-04 19:08:16 +0200

    Changed Peer.port property into int

--- gnutella/teamGML/models/peer.py
+++ gnutella/teamGML/models/peer.py
@@ -4,4 +4,4 @@ class Peer(object):
 
     def __init__(self, ip, port):
         self.ip = str(ip)       # Forced typecast, is this a good idea or bad coding?
-        self.port = str(port)
+        self.port = int(port)   # TODO check for type?

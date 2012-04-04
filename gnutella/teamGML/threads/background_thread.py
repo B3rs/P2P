8d9868309e85f69a02938ace4872b31bf2bec2f0
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2012-04-04 11:27:56 +0200

    Implement filesystem architecture for multithreading with background and ui processing. TO BE TESTED!

---
+++ gnutella/teamGML/threads/background_thread.py
@@ -0,0 +1,37 @@
+#
+# Background operations, such as received packet handling, are to be made here
+#
+__author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
+
+from threading import Thread
+import socket
+
+class BackgroundThread(Thread):
+
+    def __init__(self, ip, port):
+        super(BackgroundThread, self).__init__()
+        self.ip = ip
+        self.port = port
+
+    def run(self):
+        #print "Background thread started"
+
+        self.server_socket = None
+
+        self.server_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        )
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.server_socket.bind(
+            (self.ip, int(self.port))
+        )
+
+        self.server_socket.listen(10)
+        while 1:
+            print "Waiting for connection "
+            (socket_client, address) = self.server_socket.accept()
+            s = ServiceThread(socket_client, self.ip, self.port)
+            s.start()
+
---
+++ gnutella/teamGML/threads/ui_thread.py
@@ -0,0 +1,27 @@
+#
+# UI (and later GUI) settings and operations are to be implemented here
+#
+__author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
+
+from threading import Thread
+import sys
+
+class UIThread(Thread):
+
+    def run(self):
+        #print "UI thread started"
+        print "Gnutella by Luca Ferrari, Marco Berasni and Giovanni Lodi.\n"
+
+        while 1:
+            print "Press 1 to search a file in the network"
+            print "Press 2 to update the known peer list"
+            cmd = raw_input()
+
+            if cmd == "1":
+                print "U pressed 1 :)"
+                pass
+            elif cmd == "2":
+                print "U pressed 2 :)"
+                pass
+            else:
+                print "Invalid command! %s" %cmd

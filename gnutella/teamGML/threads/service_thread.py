bf2e0cd6e001fbda1fb36b98ad638c1874340796
Luca Ferrari <risinglf@gmail.com>
2012-04-12 12:37:37 +0200

    -Added some logging on server_thread

--- gnutella/teamGML/threads/service_thread.py
+++ gnutella/teamGML/threads/service_thread.py
@@ -6,6 +6,7 @@ from managers.peersmanager import PeersManager
 from custom_utils.formatting import *
 from custom_utils.hashing import *
 from custom_utils.sockets import *
+from custom_utils.logging import klog
 import socket
 
 class ServiceThread(Thread):
@@ -36,6 +37,8 @@ class ServiceThread(Thread):
                 ttl = str(self._socket.recv(2))
                 query = str(self._socket.recv(20))
 
+                klog("%s pid: %s %s:%s ttl: %s query: %s" % (command, pckt_id, sender_ip, sender_port, ttl, query))
+
                 if int(ttl) > 1:
                     # decrease ttl propagate the message to the peers
                     ttl = str(int(ttl) - 1)
@@ -44,6 +47,7 @@ class ServiceThread(Thread):
                     for peer in PeersManager.find_known_peers():
                         sock = connect_socket(peer.ip, peer.port)
                         sock.send(command + pckt_id + sender_ip + sender_port + ttl + query)
+                        klog("command sent to %s:%s: %s pkid:%s %s:%s ttl: %s query: %s" % (peer.ip, peer.port, command, pckt_id, sender_ip, sender_port, ttl, query))
                         sock.close()
 
 
@@ -51,8 +55,11 @@ class ServiceThread(Thread):
                     for f in FilesManager.find_files_by_query(query):
                         md5 = calculate_md5_for_file_path(f)
                         filename = f.split('/')[-1]
+                        command = "AQUE"
                         sock = connect_socket(sender_ip, sender_port)
-                        sock.send("AQUE" + pckt_id + sender_ip + sender_port + md5 + filename)
+                        sock.send(command + pckt_id + sender_ip + sender_port + md5 + filename)
+                        klog("command sent %s pkid:%s %s:%s md5: %s filename: %s" % (command, pckt_id, sender_ip, sender_port, md5, filename))
+
                         sock.close()
 
             # Received package in reply to a file research
@@ -62,7 +69,7 @@ class ServiceThread(Thread):
                 peer_ip = str(self._socket.recv(15))
                 peer_port = str(self._socket.recv(5))
                 file_md5 = str(self._socket.recv(16))
-                file_name = str(self._socket.recv(100))
+                file_name = str(self._socket.recv(100)).strip(" ")
 
                 # Add the result to the result list and show it on screen
                 self.ui_handler.add_new_result_file(file_name, peer_ip, peer_port, file_md5)
@@ -83,6 +90,8 @@ class ServiceThread(Thread):
                     for peer in PeersManager.find_known_peers():
                         sock = connect_socket(peer.ip, peer.port)
                         sock.send(command + pckt_id + sender_ip + sender_port + ttl)
+                        klog("command sent to %s:%s: %s pkid:%s %s:%s ttl: %s" % (peer.ip, peer.port, command, pckt_id, sender_ip, sender_port, ttl))
+
                         sock.close()
 
             # Received package in reply to a neighbour peer search

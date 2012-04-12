0cf2636ffcbcea8ca948a8f0f018aa3c4e8d78cb
Luca Ferrari <risinglf@gmail.com>
2012-04-12 11:39:16 +0200

    Implemented the search file method

--- gnutella/teamGML/threads/request_emitter_thread.py
+++ gnutella/teamGML/threads/request_emitter_thread.py
@@ -6,6 +6,7 @@ from models.peer import Peer
 from managers.peersmanager import PeersManager
 from custom_utils.formatting import *
 from custom_utils.hashing import generate_packet_id
+from custom_utils.sockets import connect_socket
 
 #TODO: where is the run method????????
 class RequestEmitterThread(Thread):
@@ -17,9 +18,7 @@ class RequestEmitterThread(Thread):
     def search_for_peers(self):
         print "Started query flooding for peers" # TODO write better
         for peer in PeersManager.find_known_peers():
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # useful to avoid boring address already in use
-            sock.connect((peer.ip, peer.port))
+            sock = connect_socket(peer.ip, peer.port)
             local_ip = sock.getsockname()[0]
             p_id = generate_packet_id(16) #TODO: generate
             ttl = 3
@@ -28,7 +27,13 @@ class RequestEmitterThread(Thread):
 
     def search_for_files(self, query):
         print "Started query flooding for files: %s" %query
-        print "TODO"
+        for peer in PeersManager.find_known_peers():
+            sock = connect_socket(peer.ip, peer.port)
+            local_ip = sock.getsockname()[0]
+            p_id = generate_packet_id(16) #TODO: generate
+            ttl = 3
+            sock.send("QUER" + p_id + format_ip_address(local_ip) + format_port_number(self.local_port) + str(ttl) + format_query(query))
+            sock.close()
 
     def download_file(self, peer_ip, peer_port, md5):
         print "Download....."

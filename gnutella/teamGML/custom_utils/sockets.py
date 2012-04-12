0c25081d9da621ae4f8e6e8c4a211b846afad26a
Luca Ferrari <risinglf@gmail.com>
2012-04-12 11:54:09 +0200

    -Added an integer conversion to sock.connect for the port parameter

--- gnutella/teamGML/custom_utils/sockets.py
+++ gnutella/teamGML/custom_utils/sockets.py
@@ -1,8 +1,10 @@
 __author__ = 'luca'
 
+import socket
+
 
 def connect_socket(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # useful to avoid boring address already in use
-    sock.connect((ip, port))
+    sock.connect((ip, int(port)))
     return sock

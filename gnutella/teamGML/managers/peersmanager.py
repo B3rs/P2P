9282c697af1a925bdc36ebfeafff60b6e6f1ae4a
Luca Ferrari <risinglf@gmail.com>
2012-04-13 10:23:48 +0200

    -Added some utils methods to PeerManager (same peer check and is_know_peer)

--- gnutella/teamGML/managers/peersmanager.py
+++ gnutella/teamGML/managers/peersmanager.py
@@ -1,7 +1,7 @@
 __author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
 
 from models.peer import Peer
-from custom_utils.formatting import format_ip_address
+from custom_utils.formatting import *
 
 PEERS = [] #TODO: remove this into a singleton instance of PeersManager
 
@@ -13,15 +13,19 @@ class PeersManager(object):
         return PEERS
 
     @classmethod
-    def is_known_peer(cls, ip):
+    def is_known_peer(cls, ip, port):
         for peer in PEERS:
-            if peer.ip == format_ip_address(ip):
+            if peer.ip == format_ip_address(ip) and peer.port == format_port_number(port):
                 return True
         return False
+
+    @classmethod
+    def are_same_peer(cls, peer1, peer2):
+        return peer1.ip == peer2.ip and peer1.port == peer2.port
 
     # @returns add a new known
     @classmethod
     def add_new_peer(cls, peer_ip, peer_port):
         peer = Peer(peer_ip, peer_port)
-        if not PeersManager.is_known_peer(peer_ip):
+        if not PeersManager.is_known_peer(peer_ip, peer_port):
             PEERS.append(peer)

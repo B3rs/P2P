6310914d0033418fddeca49f2c5ba70e0f9a09b3
Luca Ferrari <risinglf@gmail.com>
2012-04-13 11:19:49 +0200

    -The pkid on query is generate once

--- gnutella/teamGML/threads/request_emitter_thread.py
+++ gnutella/teamGML/threads/request_emitter_thread.py
@@ -29,10 +29,11 @@ class RequestEmitterThread(Thread):
 
     def search_for_files(self, query):
         print "Started query flooding for files: %s" %query
+        p_id = generate_packet_id(16)
+
         for peer in PeersManager.find_known_peers():
             sock = connect_socket(peer.ip, peer.port)
             local_ip = sock.getsockname()[0]
-            p_id = generate_packet_id(16)
             PacketsManager.add_new_generated_packet(p_id)
             ttl = 3
             sock.send("QUER" + p_id + format_ip_address(local_ip) + format_port_number(self.local_port) + format_ttl(ttl) + format_query(query))

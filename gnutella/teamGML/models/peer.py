67c9ed658134b45a3886aa87ce9ef8edee9b3542
Luca Ferrari <risinglf@gmail.com>
2012-04-13 10:32:21 +0200

    -Format the number on Peer constructor -The same peer bug is now fixed

--- gnutella/teamGML/models/peer.py
+++ gnutella/teamGML/models/peer.py
@@ -1,10 +1,10 @@
 __author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
 
-from custom_utils.formatting import format_ip_address
+from custom_utils.formatting import format_ip_address, format_port_number
 
 
 class Peer(object):
 
     def __init__(self, ip, port):
-        self.ip = format_ip_address(ip)       # Forced typecast, is this a good idea or bad coding?
-        self.port = int(port)   # TODO check for type?
+        self.ip = format_ip_address(ip)
+        self.port = format_port_number(port)

4732cba1b58ef905bff3fe4910dff3fdd0cff66b
Marco Bersani <tachetefogo@gmail.com>
2012-04-15 17:26:37 +0200

    Added file model and updated the filesmanager to use that module.

--- gnutella/teamGML/managers/filesmanager.py
+++ gnutella/teamGML/managers/filesmanager.py
@@ -1,6 +1,7 @@
 __author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
 import os
 from custom_utils import hashing
+from models.file import File
 
 # TODO: WARNING!
 # SHARED_PATH = "../shared"
@@ -8,9 +9,18 @@ from custom_utils import hashing
 # because the method it's called form another file in a different location...
 # TODO find a way to evaluate the path in a way that is transparent to the caller of the method
 SHARED_PATH = "../shared"
-
+FILES = []
 
 class FilesManager(object):
+
+    @classmethod
+    def load_files(cls):
+        for dirname, dirnames, filenames in os.walk(SHARED_PATH):
+            for filename in filenames:
+                path = dirname + "/" + filename
+                md5 = hashing.calculate_md5_for_file_path(path)
+                file = File(dirname, filename, md5)
+                FILES.append(file)
 
     # @returns array with matches path
     @classmethod
@@ -21,28 +31,18 @@ class FilesManager(object):
         # Loop in the shared files directory and look for occurrence
         matches = []
 
-        for dirname, dirnames, filenames in os.walk(shared_path):
-            for filename in filenames:
-                if filename.lower().find(query) != -1:
-                    matches.append(os.path.join(dirname, filename))
-
+        for file in FILES:
+            if file.filename.lower().find(query) != -1:
+                matches.append(file)
         return matches
 
-    # TODO efficiency!!!
     @classmethod
     def find_file_by_md5(cls, md5):
-        for dirname, dirnames, filenames in os.walk(SHARED_PATH):
-            for filename in filenames:
-                path = dirname + "/" + filename
-                fileMd5 = hashing.calculate_md5_for_file_path(path)
-                if fileMd5 == md5:
-                    return path
+        for file in FILES:
+            if file.md5 == md5:
+                return file
         return False
 
     @classmethod
     def shared_files(cls):
-        results = []
-        for dirname, dirnames, filenames in os.walk(SHARED_PATH):
-            for filename in filenames:
-                results.append(filename)
-        return results
+        return FILES

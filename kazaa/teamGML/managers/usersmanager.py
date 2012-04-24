83b00bb8eca838e3766765232661c4faee0b36c8
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2012-04-24 09:51:15 +0200

    Added super node flag in the UsersManager

--- kazaa/teamGML/managers/usersmanager.py
+++ kazaa/teamGML/managers/usersmanager.py
@@ -5,6 +5,7 @@ from custom_utils import hashing
 from models.user import User
 
 MY_SESSIONID = ""
+IS_SUPER_NODE = ""
 
 class UsersManager(object):
 
@@ -49,3 +50,11 @@ class UsersManager(object):
     @classmethod
     def set_my_session_id(cls, session_id):
         MY_SESSIONID = session_id
+
+    @classmethod
+    def is_super_node(cls):
+        return IS_SUPER_NODE
+
+    @classmethod
+    def set_is_super_node(cls, is_super_node):
+        IS_SUPER_NODE = is_super_node

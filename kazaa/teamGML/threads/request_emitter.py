4a5655f509bf8b808e93d3acd30200bd255f7a29
Luca Ferrari <risinglf@gmail.com>
2012-05-02 10:48:19 +0200

    A local file search result is not added to the results list

--- kazaa/teamGML/threads/request_emitter.py
+++ kazaa/teamGML/threads/request_emitter.py
@@ -80,7 +80,8 @@ class RequestEmitter(object):
             # Perform a local search here.
             for file in FilesManager.find_files_by_query(query):
                 owner = UsersManager.find_user_by_session_id(file.session_id)
-                self.ui_handler.add_new_result_file(file.filename, owner.ip, owner.port, encode_md5(file.hash))
+                if owner:
+                    self.ui_handler.add_new_result_file(file.filename, owner.ip, owner.port, encode_md5(file.hash))
 
             # ...and send a QUER in the network, its results will be handled properly and should not interfere with
             # the ones of the local search

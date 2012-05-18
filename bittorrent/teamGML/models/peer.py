1eeec1cc49103d3193e50639e37e0c50ec6838ce
Luca Ferrari <luca.ferrari@risingbits.com>
2012-05-18 16:50:20 +0200

    Added class method to Peer model to get a local Peer object representation

--- bittorrent/teamGML/models/peer.py
+++ bittorrent/teamGML/models/peer.py
@@ -1,7 +1,7 @@
 __author__ = 'LucaFerrari MarcoBersani GiovanniLodi'
 
 from custom_utils.formatting import format_ip_address, format_port_number
-
+from configurations import PORT
 
 class Peer(object):
 
@@ -11,3 +11,8 @@ class Peer(object):
 
     def __str__(self):
         return "%s:%s" % (self.ip, self.port)
+
+
+    @classmethod
+    def get_local_peer(cls):
+        return Peer("127.0.0.0", PORT)
